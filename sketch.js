d43b88a7c2c90c42e0856e1f79fa385e538aca37
zeal2407 <77246154+zeal2407@users.noreply.github.com>
2021-03-18 20:11:46 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var gameState = 0;
+var playerCount;
+var database;
+var form, player, game;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  background("white");
+  
+  
+}
+
+
